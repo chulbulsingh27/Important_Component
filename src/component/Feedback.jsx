aba871cb3aba6908ec284dc19e71397eea2024a4
chulbul <chulbul.singh@awign.com>
2023-03-09 20:24:45 +0530

    commiting new code

--- src/component/Feedback.jsx
+++ src/component/Feedback.jsx
@@ -1,12 +1,50 @@
 import React from 'react';
+import { useState } from 'react';
 import { RxCross2 } from "react-icons/rx";
 import { MdOutlineWifi } from "react-icons/md";
 import { TbStar } from "react-icons/tb";
 
 export default function Feedback() {
+    // const data ={name:"",email:""};
+    // const [inputData,setInputData] = useState(data);
+    // function handleData(e){
+    //     setInputData({...inputData, [e.target.name]:e.taget.value})
+    //     console.log(inputData);
+    // }
+    // function handleSubmit (e){
+    //     e.preventDefault();
+    //     if(inputData.name){
+    //         alert("Given fields are compulsory")
+    //     }
+
+    // }
+    const [form, setForm] = React.useState({
+        textValue: '',
+        // password: '',
+      });
     
+      const handleChange = (event) => {
+        setForm({
+          ...form,
+          [event.target.id]: event.target.value,
+          
+        });
+        console.log(form);
+      };
+    
+      const handleSubmit = (event) => {
+        event.preventDefault();
+    
+        alert(form.textValue );
+      };
+    
+    
+    
+
     return (
-        <div className='h-[1000px] w-full bg-black   flex justify justify-end py-96  p-16'>
+        <form onSubmit = {handleSubmit}>
+        
+        <div className='h-[1000px] w-full bg-black   flex justify justify-end py-96  p-16 '>
             <div className='w-[700px] h-[510px] bg-white px-10 py-8 rounded '>
                 <div className='relative justify start text-xl flex  items-center text-black pb-3 '>
                     <div className=''> <MdOutlineWifi size="1.64rem" /></div>
@@ -16,11 +54,11 @@ export default function Feedback() {
                 <hr className='mt-2' />
                 <div className='text-md py-3 opacity-60'>Your feedback is invaluable to us. It helps us to improve our services.</div>
                 <div className='relative space-x-8 py-3 opacity-70'>
-                    <div class="absolute left-0">
+                    <div className="absolute left-0">
                         <p>Ease of using</p>
                     </div>
-                    <div className='absolute right-0 flex space-x-4' >
-                        <div className=''> <TbStar size="1.64rem" /></div>
+                    <div className='absolute right-0 flex space-x-4 cursor-pointer' >
+                        <div className=''> <TbStar size="1.64rem"/></div>
                         <div className=''> <TbStar size="1.64rem" /></div>
                         <div className=''> <TbStar size="1.64rem" /></div>
                         <div className=''> <TbStar size="1.64rem" /></div>
@@ -28,10 +66,10 @@ export default function Feedback() {
                     </div>
                 </div>
                 <div className='relative space-x-8 py-4 opacity-70 '>
-                    <div class="absolute left-0 py-4">
+                    <div className="absolute left-0 py-4">
                         <p >Navigation</p>
                     </div>
-                    <div className='absolute right-0 flex py-4 space-x-4' >
+                    <div className='absolute right-0 flex py-4 space-x-4 cursor-pointer' >
                         <div className=''> <TbStar size="1.64rem" /></div>
                         <div className=''> <TbStar size="1.64rem" /></div>
                         <div className=''> <TbStar size="1.64rem" /></div>
@@ -39,9 +77,9 @@ export default function Feedback() {
                         <div className=''> <TbStar size="1.64rem" /></div>
                     </div>
                 </div>
-                <div class="col-span-6 sm:col-span-3 py-10">
-                    <label for="last_name" class="block text-md  text-gray-700">Any special recommendations</label>
-                    <textarea type="text" placeholder="Type here ..." class="mt-2 py-2 border border-black p-2  focus:ring-gray-500 focus:border-gray-500 block w-full  h-32  xl:text-xl  border-black-400  "></textarea>
+                <div className="col-span-6 sm:col-span-3 py-10">
+                    <label  className="block text-md  text-gray-700">Any special recommendations</label>
+                    <textarea type="text" placeholder="Type here ..." id ='name' value = {form.name} onChange ={handleChange} className="mt-2 py-2 border border-black p-2  focus:ring-gray-500 focus:border-gray-500 block w-full  h-32  xl:text-xl  border-black-400  "></textarea>
                 </div>
 
                 {/* <div className=' flex space-x-8 py-4 opacity-70 justify-end'>
@@ -52,16 +90,17 @@ export default function Feedback() {
                 </div> */}
 
                 <div className="flex py-6 justify-end space-x-2 ">
-                    <div className="  text-blue-800">
+                    <div className="  text-[#470850]">
                         <button className='px-7 py-2  rounded-md'>Maybe Later</button>
                     </div>
 
                     <div className=" right-0 text-white">
-                        <button  className='bg-blue-800 px-7 py-2 rounded-md'>Submit</button>
+                        <button className='bg-[#470850] px-7 py-2 rounded-md '>Submit</button>
                     </div>
                 </div>
             </div>
         </div>
+        </form>
 
     )
 }
